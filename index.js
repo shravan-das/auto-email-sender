32c421377f5b56d49b600653e19d9b6d8391bdd2
Shravan <ishravan919@gmail.com>
2023-07-05 22:30:26 +0530

    small changes

--- index.js
+++ index.js
@@ -208,7 +208,7 @@ app.get('/processEmails', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
